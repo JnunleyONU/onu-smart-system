405ed4d51fecae35d3fbac59a586cd49013ca176
Jamir Nunley <j-nunley@onu.edu>
2023-11-01 14:44:25 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/onu_smart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.onu_smart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InspirationApp());
+}
+
+class InspirationApp extends StatelessWidget {
+  const InspirationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const LoginPage(),
+      theme: ThemeData(
+        primaryColor: Colors.orange,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Launch'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Perform login logic here
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MainPage(),
+                  ),
+                );
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SMART System Control'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LargeButton('Fountain Of Inspiration'),
+            LargeButton('3D Print'),
+            LargeButton('Smart Tour'),
+            LargeButton('Options'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LargeButton extends StatelessWidget {
+  final String text;
+
+  LargeButton(this.text, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          // Implement button actions here
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.orange),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
