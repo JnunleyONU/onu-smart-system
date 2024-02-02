26bc6ecf34641675dee4d2796fe1b2e2f7ab28fc
Jared Swartz <j-swartz.3@onu.edu>
2024-02-01 21:44:46 -0500

    Created functions to import json of student list into masterStudentList and then sort by major and display

--- lib/pages/read_examples.dart
+++ lib/pages/read_examples.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:onu_smart/constants.dart';
@@ -29,35 +27,18 @@ class _ReadExamplesState extends State<ReadExamples> {
       final data = event.snapshot.value;
       setState(() {
         _displayText = "Sheet1: $data";
-        print("test" + _displayText);
+        // print("test" + _displayText);
       });
     });
   }
 
-  Future<void> readProfileData() async {
-    DatabaseReference ref = FirebaseDatabase.instance
-        .ref("1eBfu_F_M6x46hjeJrIZj84gxWmCOHjXrLA6r0Xl3Als/Sheet1");
+  // Future<void> readProfileData() async {
+  //   DatabaseReference ref = FirebaseDatabase.instance.ref("$excelRoot/Sheet1");
 
-    final DataSnapshot? snapShot = await ref.get();
-
-    Map<String, dynamic> data = jsonDecode(jsonEncode(snapShot?.value));
-    List testList = [];
-
-    final keys = data.keys;
-    keys.forEach((element) {
-      testList.add(element);
-    });
-
-    for (var i = 0; i < 3; i++) {
-      print(testList[i].toString());
-    }
-    // final jsonString = JSON.stri;
-    // print(data);
-    // final jsonFull = jsonDecode(jsonString);
-    // var value = data.values;
-
-    // value.forEach(print);
-  }
+  //   final DataSnapshot snapShot = await ref.get();
+  //   rawData = jsonDecode(jsonEncode(snapShot.value));
+  //   createStudents(rawData);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +55,7 @@ class _ReadExamplesState extends State<ReadExamples> {
           ElevatedButton(
               onPressed: () {
                 // print(_displayText
-                readProfileData();
+                // readProfileData();
               },
               child: const Text("Read Data"))
         ]),
