068ff606566a0bda65713f0509390c21c688c635
Flyingtaco9000 <a-brown.27@onu.edu>
2024-03-05 11:37:14 -0500

    Printer Functionallity on ONU network works now SelectFile Function lets you select any gcode file you have sent to the server Any user should be able to access the octoprint server via app now

--- lib/pages/Printer_home.dart
+++ lib/pages/Printer_home.dart
@@ -16,8 +16,9 @@
 //          
 // Cancel Job : 'cancel' (WORKS)
 // Start Job : 'start' (Work in Progress) (most likely wont be used in final product because file select allows print start as well)
-// Select File: 'select' (DEMO WORKS) (will be used to select files and start prints, see documentation for details)
+// Select File: 'select' (will be used to select files and start prints, see documentation for details)
 // Pause Job : 'pause' (WORKS) (See documentation for additonal parameters)
+// 
 
 // See "Job Operations" on Octoprint REST API Documentation for details on JSON File encoding
 //-------------------------------------------------------------------------------------------------------------------
@@ -34,11 +35,18 @@
 //
 //------------------------------------------------------------------------------------------------------------------
 
-//--------------------------Wishful Thinking ----------------------------------------------------------------------
+//--------------------------Wishful Thinking -----------------------------------------------------------------------
 // Allow user to upload files from their device to start print (will most likely not happen for this project but would be cool ya know?)
+//------------------------------------------------------------------------------------------------------------------
+
+//--------------------------Files Currently on Server -------------------------------------------------------------
+//   Add the names of any files uploaded to the server, the name is what is passed into the select print function 
+//  "PolarBearCapstone.gcode" - 2D Model of Polar Bear
 //
-
-
+//
+//
+//
+//-----------------------------------------------------------------------------------------------------------------
 
 import 'package:flutter/material.dart';
 import 'package:onu_smart/constants.dart';
@@ -48,7 +56,7 @@ import 'package:http/http.dart' as http;
 
 
 //Global Variables
-String serverIP = "192.168.129.88"; //Ip address of current octopi server
+String serverIP = "192.168.23.241"; //Ip address of current octopi server
 String apiKey = "0D6B765B66C94A75B89233E93946C0EA"; //api key that allows for application to control server without user authentication
 
 
@@ -79,18 +87,15 @@ class PrinterHome extends StatelessWidget {
                 onPressed: () {
                   //Start print function code
 
-                  //startJob();
-
-                   startDemo();   //----THIS WORKS ---
-
-                  //selectPrint("AlexBrown_M2BWindTurbineBlade.gcode");
+                  
+                  selectPrint("PolarBearCapstone.gcode");
 
                 },
                 style: ElevatedButton.styleFrom(
                   foregroundColor: Colors.black,
                   backgroundColor: onuOrange, // Set the font color to black
                 ),
-                child: const Text('Start Demo Print'),
+                child: const Text('Start Polar Bear 2D'),
               ),
               ElevatedButton(
                 onPressed: () {
@@ -110,15 +115,14 @@ class PrinterHome extends StatelessWidget {
               ElevatedButton(
                 onPressed: () {
 
-
-
+                  resumeJob();
 
                 },
                 style: ElevatedButton.styleFrom(
                   foregroundColor: Colors.black,
                   backgroundColor: onuOrange, // Set the font color to black
                 ),
-                child: const Text('Monitor (WIP)'),
+                child: const Text('Resume Print'),
               ),
               ElevatedButton(
                 onPressed: () {
@@ -141,7 +145,6 @@ class PrinterHome extends StatelessWidget {
 }
 
 
-//still need to test this function, only change is variable filename and ip address of server
 Future<http.Response> selectPrint(String filename) async {
 
   String address = "http://$serverIP/api/files/local/$filename";
@@ -164,11 +167,10 @@ Future<http.Response> selectPrint(String filename) async {
 
 }
 
-
-//Ip address will be changed once the actual server is configured
+//Not used in final implementation, used for debugging
 Future<http.Response> startDemo() async{
 
-String hostName = "http://192.168.228.221/api/files/local/AlexBrown_M2BWindTurbineBlade.gcode";
+String hostName = "http://192.168.23.241/api/files/local/PolarBearCapstone.gcode";
 
 var url = Uri.parse(hostName);
 var headers = {
@@ -188,28 +190,8 @@ return response;
 
 }
 
-
-/* Starts currently selected file to be printed, will most likely not be used because a file needs to be selected to start
-*/
-Future<http.Response> startJob() async {
- var url = Uri.parse('http://3dcapstone.local');
- var headers = {
-    "Content-Type": "application/json",
-    "X-Api-Key": "0D6B765B66C94A75B89233E93946C0EA"
- };
- var body = json.encode({"command" : "start"});
-
- var response = await http.post(url, headers: headers, body: body);
-
-  //print('Status code: ${response.statusCode}');
-  //print('Response body: ${response.body}');
-
- return response;
-}
-
-
 Future<http.Response> pauseJob() async {
-  String urlName = "http://192.168.228.221/api/job";
+  String urlName = "http://$serverIP/api/job";
  var url = Uri.parse(urlName);
  var headers = {
     "Content-Type": "application/json",
@@ -224,8 +206,24 @@ Future<http.Response> pauseJob() async {
  return response;
 }
 
+Future<http.Response> resumeJob() async {
+  String urlName = "http://$serverIP/api/job";
+ var url = Uri.parse(urlName);
+ var headers = {
+    "Content-Type": "application/json",
+    "X-Api-Key": "0D6B765B66C94A75B89233E93946C0EA"
+ };
+
+ var body = json.encode({"command" : "pause",
+                        "action" : "resume"});
+
+ var response = await http.post(url, headers: headers, body: body);
+
+ return response;
+}
+
 Future<http.Response> cancelJob() async {
-  String domainName = "http://192.168.228.221/api/job";
+  String domainName = "http://$serverIP/api/job";
 
   var url = Uri.parse(domainName);
    var headers = {
@@ -239,7 +237,3 @@ Future<http.Response> cancelJob() async {
 
  return response;
 }
-
-// GET A WIRED CONNECTION (according to IT, I think it will be just fine on wifi IMO) however this will not be possible in the app
-//going to test both methods but unless theres an ethernet port in the maker space I want to stick with ethernet
-//also Chris needs to be considered and talked to
